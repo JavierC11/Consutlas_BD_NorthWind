ef1e7d4040653024797a73e25cec1c16eaec207e
Javier Castillo <JavC@MacBooks-MacBook-Air.local>
2022-11-06 08:21:30 -0600

    adding some extra querys

--- consultas.sql
+++ consultas.sql
@@ -319,3 +319,287 @@ WHERE EXISTS (
 GROUP BY CU.COMPANYNAME
 ORDER BY COUNT(OD1.ORDERID) DESC
 FETCH FIRST 1 ROW ONLY;
+
+
+
+--21. (SUBCONSULTA - CLASICA) - Dar el nombre del proveedor y los números de órdenes en donde los clientes 
+--hayan comprado sus productos distribuidos donde por lo menos un cliente los haya comprado en junto y 
+--que sus productos inicien con “L” y terminen con “a”.
+SELECT S.COMPANYNAME NOMBRE_PROVEEDOR, O.ORDERID, P.PRODUCTNAME NOMBRE_PRODUCTO
+FROM CUSTOMERS C ,ORDERS O , ORDERDETAILS OD, PRODUCTS P, SUPPLIERS S 
+WHERE
+    C.CUSTOMERID = O.CUSTOMERID AND
+    OD.ORDERID = O.ORDERID AND
+    P.PRODUCTID = OD.PRODUCTID AND
+    S.SUPPLIERID = P.SUPPLIERID AND
+    P.PRODUCTID IN (
+                    SELECT P.PRODUCTID
+                    FROM PRODUCTS P
+                    WHERE P.PRODUCTNAME LIKE 'L%a'
+                    
+    )
+AND C.CUSTOMERID IS NOT NULL
+ORDER BY O.ORDERID ASC;
+
+
+ 
+
+--22. (SUBCONSULTA - MATEMATICA) - Dar el nombre donde todas las ordenes estén entre las fechas 01/01/96 al 31/12/96, pero que a su vez 
+--el promedio del precio de cada orden asociada sea el menor que su total.
+
+SELECT O.ORDERID, O.ORDERDATE, SUM((OD.UNITPRICE * OD.QUANTITY)*(1-OD.DISCOUNT)) SUMA_ORDEN 
+FROM ORDERS O
+INNER JOIN ORDERDETAILS OD ON O.ORDERID = OD.ORDERID
+AND O.ORDERDATE BETWEEN '01/01/1996' AND '31/12/1996'
+HAVING SUM((OD.UNITPRICE * OD.QUANTITY)*(1-OD.DISCOUNT)) > (
+                                                        SELECT AVG(SUM((OD1.UNITPRICE * OD1.QUANTITY)*(1-OD1.DISCOUNT)))
+                                                        FROM ORDERS O1
+                                                        INNER JOIN ORDERDETAILS OD1 ON O1.ORDERID = OD1.ORDERID
+                                                        AND O1.REQUIREDDATE BETWEEN '01/01/1996' AND '31/12/1996'
+                                                        GROUP BY O1.ORDERID
+)
+GROUP BY O.ORDERID, O.ORDERDATE
+ORDER BY SUMA_ORDEN DESC;
+ 
+
+--23. (LIBRE) - Dar el nombre del proveedor, donde su producto haya sido comprado, y que su promedio
+-- sea menor a la suma total de todas sus ordenes.
+SELECT  S.COMPANYNAME, SUM((OD.UNITPRICE*OD.QUANTITY)*(1-OD.DISCOUNT)) TOTAL,  AVG((OD.UNITPRICE*OD.QUANTITY)*(1-OD.DISCOUNT)) PROMEDIO 
+FROM ORDERDETAILS OD
+INNER JOIN PRODUCTS P ON OD.PRODUCTID = P.PRODUCTID
+INNER JOIN SUPPLIERS S ON P.SUPPLIERID = S.SUPPLIERID
+WHERE OD.PRODUCTID IS NOT NULL
+HAVING SUM((OD.UNITPRICE*OD.QUANTITY)*(1-OD.DISCOUNT)) <(
+
+)AVG((OD.UNITPRICE*OD.QUANTITY)*(1-OD.DISCOUNT)) < 
+GROUP BY  S.COMPANYNAME;
+ 
+--24. (SUBCONSULTA - CLASICA) - Dar el nombre del producto vendido a un precio igual al definido, y que ese producto aparezca como mínimo 4 ordenes.
+SELECT P1.PRODUCTNAME, COUNT(OD.ORDERID), P1.UNITPRICE, OD.UNITPRICE, OD.DISCOUNT
+FROM ORDERDETAILS OD, PRODUCTS P1
+WHERE
+P1.PRODUCTID = OD.PRODUCTID AND
+P1.PRODUCTID IN (
+                    SELECT P.PRODUCTID
+                    FROM PRODUCTS P, ORDERDETAILS OD1
+                    WHERE P.PRODUCTID = OD1.PRODUCTID
+                    AND P.UNITPRICE = OD.UNITPRICE
+                    GROUP BY P.PRODUCTID
+                    )
+GROUP BY P1.PRODUCTNAME, P1.UNITPRICE, OD.UNITPRICE, OD.DISCOUNT
+HAVING COUNT(DISTINCT OD.ORDERID) >= 4
+ORDER BY COUNT(OD.ORDERID) DESC;
+
+
+--25. (LIBRE) - Mostrar los proveedores (nombre) con más de 10000 en ventas.
+SELECT S.COMPANYNAME, SUM(OD.UNITPRICE * OD.QUANTITY * (1-OD.DISCOUNT)) TOTAL
+FROM SUPPLIERS S 
+INNER JOIN PRODUCTS P ON S.SUPPLIERID = P.SUPPLIERID 
+INNER JOIN ORDERDETAILS OD ON P.PRODUCTID = OD.PRODUCTID
+GROUP BY S.COMPANYNAME
+HAVING SUM(OD.UNITPRICE * OD.QUANTITY * (1-OD.DISCOUNT)) > 10000
+ORDER BY TOTAL DESC;
+ 
+
+--26. (CONSULTA CLASICA) - Calcular el total de ventas por empleado ordenando de mayor a menor.
+SELECT E.FIRSTNAME || ' ' || E.LASTNAME "Nombre Empleado", SUM((OD.UNITPRICE * OD.QUANTITY)*(1-OD.DISCOUNT)) "Total Ventas"
+FROM EMPLOYEES E, ORDERS O, ORDERDETAILS OD
+WHERE
+    E.EMPLOYEEID = O.EMPLOYEEID AND
+    O.ORDERID = OD.ORDERID
+GROUP BY E.FIRSTNAME || ' ' || E.LASTNAME   
+ORDER BY SUM((OD.UNITPRICE * OD.QUANTITY)*(1-OD.DISCOUNT)) DESC;
+ 
+
+--27. (SUBCONSULTA - MATEMATICA) - Dar el nombre de los empleados que hayan 
+-- hecho al menos 2 ordenes y que el promedio de esas ordenes sea menor a la 
+-- suma total de la ordenes.
+SELECT E.FIRSTNAME||' '||E.LASTNAME NOMBRE_EMPLEADO, COUNT(DISTINCT O.ORDERID)
+FROM ORDERDETAILS OD
+INNER JOIN ORDERS O ON OD.ORDERID = O.ORDERID
+INNER JOIN EMPLOYEES E ON O.EMPLOYEEID = E.EMPLOYEEID
+
+HAVING COUNT(DISTINCT O.ORDERID) > 2
+AND SUM((OD.UNITPRICE * OD.QUANTITY)*(1-OD.DISCOUNT)) > (
+                                                            SELECT AVG(SUM((OD1.UNITPRICE * OD1.QUANTITY)*(1-OD1.DISCOUNT)))
+                                                            FROM ORDERDETAILS OD1
+                                                            GROUP BY OD1.ORDERID
+)
+GROUP BY E.FIRSTNAME||' '||E.LASTNAME
+ORDER BY COUNT(DISTINCT O.ORDERID) DESC;
+
+
+/*28
+Dar el nombre del proveedor y los números de órdenes en donde los clientes 
+hayan comprado sus productos distribuidos donde por lo menos un cliente los 
+haya comprado en junto y que sus productos inicien con “L” y terminen con “a”.
+*/
+SELECT S.COMPANYNAME, COUNT(OD.ORDERID)
+FROM CUSTOMERS CU
+INNER JOIN ORDERS O ON CU.CUSTOMERID = O.CUSTOMERID
+INNER JOIN ORDERDETAILS OD ON O.ORDERID = OD.ORDERID
+INNER JOIN PRODUCTS P ON OD.PRODUCTID = P.PRODUCTID
+INNER JOIN SUPPLIERS S ON P.SUPPLIERID = S.SUPPLIERID
+WHERE CU.CUSTOMERID IS NOT NULL
+AND P.PRODUCTNAME LIKE 'L%a'
+GROUP BY S.COMPANYNAME
+
+--si pide en vez de el conteo el id de las ordenes
+SELECT S.COMPANYNAME, O.ORDERID, P.PRODUCTNAME
+FROM SUPPLIERS S, PRODUCTS P, ORDERDETAILS OD, ORDERS O, CUSTOMERS C
+WHERE
+S.SUPPLIERID = P.SUPPLIERID AND
+P.PRODUCTID = OD.PRODUCTID AND
+OD.ORDERID = O.ORDERID AND
+O.CUSTOMERID = C.CUSTOMERID AND
+P.PRODUCTNAME LIKE 'L%a'
+
+
+/* 28
+Dar el nombre y apellido del empleado que realizo uno o más órdenes y que en 
+las ordenes contenga al menos un producto de dos o más categorías diferentes; en 
+la orden tienen que tener un total igual o superior a Q10.00 y que el promedio 
+del precio del producto de cada categoría que está en la orden sea superior 
+al total encontrado en la orden (Q10.00) anteriormente.
+*/ 
+SELECT E1.FIRSTNAME || ' ' || E1.LASTNAME, O1.ORDERID
+FROM EMPLOYEES E1
+INNER JOIN ORDERS O1 ON E.EMPLEYEEID = O.EMPLOYEEID
+INNER JOIN ORDERDETAILS OD1 ON O1.ORDERID = OD1.ORDERID
+INNER JOIN PRODUCTS P1 ON OD1.PRODUCTID = P1.PRODUCTID
+INNER JOIN CATEGORIES C1 ON P1.CATEGORYID = C1.CATEGORYID
+WHERE O.EMPLOYEEID IS NOT NULL
+AND SUM((OD1.UNITPRICE * OD1.QUANTITY)*(1-OD1.DISCOUNT)) >=10
+AND SUM((OD1.UNITPRICE * OD1.QUANTITY)*(1-OD1.DISCOUNT)) > (
+							SELECT AVG(P.UNITPRICE) PROMEDIO_PRODUCTO	
+							FROM PRODUCTS P
+							INNER JOIN CATEGORIES C ON P.CATEGORYID = C.CATEGORYID
+							WHERE C.CATEGORYID = C1.CATEGORYID
+							GROUP BY AVG(P.UNITPRICE)	
+)
+GROUP BY E1.FIRSTNAME || ' ' || E1.LASTNAME							
+HAVING COUNT(DISTINCT C1.CATEGORYNAME) >= 2
+
+
+/* 29
+Dar el nombre del producto que cumplan con las siguientes determinaciones, 
+el precio del producto tiene que ser menor que el promedio del precio total de cada categoría, 
+el producto tiene que estar al menos en una orden y la sumatoria total de esa orden tienen que ser mayor a Q50.00.
+*/
+SELECT P1.PRODUCTNAME, P1.UNITPRICE
+FROM ORDERDETAILS OD1
+INNER JOIN PRODUCTS P1 ON OD1.PRODUCTID = P1.PRODUCTID
+INNER JOIN CATEGORIES C1 ON P1.CATEGORYID = C1.CATEGORYID
+WHERE OD1.PRODUCTID IS NOT NULL
+AND P1.UNITPRICE < ALL (                    
+                    SELECT AVG(P.UNITPRICE) PROMEDIO_CATEGORIAS
+                    FROM ORDERDETAILS OD
+                    INNER JOIN PRODUCTS P ON OD.PRODUCTID = P.PRODUCTID
+                    INNER JOIN CATEGORIES C ON P.CATEGORYID = C.CATEGORYID
+                    GROUP BY C.CATEGORYID
+)
+GROUP BY P1.PRODUCTNAME, P1.UNITPRICE
+HAVING SUM((OD1.UNITPRICE * OD1.QUANTITY)*(1-OD1.DISCOUNT)) > 50
+ORDER BY PRODUCTNAME, P1.UNITPRICE DESC
+
+
+/* 30
+Dar el nombre del cliente, sus códigos de todas las ordenes asociadas, el total de cada 
+orden y el nombre completo del empleado que lo realizo; en donde su código id del 
+producto tiene al menos dos letras “A” o dos letras “E”.
+*/
+SELECT CU.COMPANYNAME, O.ORDERID, SUM((OD.UNITPRICE * OD.QUANTITY)*(1-OD.DISCOUNT)) TOTAL_ORDEN,
+E.FIRSTNAME ||' '|| E.LASTNAME NOMBRE_EMPLEADO
+FROM PRODUCTS P
+INNER JOIN ORDERDETAILS OD ON P.PRODUCTID = OD.PRODUCTID
+INNER JOIN ORDERS O ON OD.ORDERID = O.ORDERID
+INNER JOIN CUSTOMERS CU ON O.CUSTOMERID = CU.CUSTOMERID
+INNER JOIN EMPLOYEES E ON O.EMPLOYEEID = E.EMPLOYEEID
+WHERE P.PRODUCTID LIKE '%A%A%'
+OR P.PRODUCTID LIKE 'A%%A'
+OR P.PRODUCTID LIKE '%E%E%'
+OR P.PRODUCTID LIKE 'E%%E'
+GROUP BY CU.COMPANYNAME, O.ORDERID,
+E.FIRSTNAME ||' '|| E.LASTNAME 
+
+
+/* 31
+Dar el nombre donde todas las ordenes estén entre las fechas 01/01/96 al 31/12/96, 
+pero que a su vez el promedio del precio de cada orden asociada sea el menor que su total.
+*/
+
+SELECT O.ORDERID, O.ORDERDATE, SUM((OD.UNITPRICE * OD.QUANTITY)*(1-OD.DISCOUNT)) SUMA_ORDEN 
+FROM ORDERS O
+INNER JOIN ORDERDETAILS OD ON O.ORDERID = OD.ORDERID
+WHERE O.ORDERDATE BETWEEN '01/01/1996' AND '31/12/1996'
+HAVING SUM((OD.UNITPRICE * OD.QUANTITY)*(1-OD.DISCOUNT)) > (
+                                                        SELECT AVG(SUM((OD1.UNITPRICE * OD1.QUANTITY)*(1-OD1.DISCOUNT)))
+                                                        FROM ORDERS O1
+                                                        INNER JOIN ORDERDETAILS OD1 ON O1.ORDERID = OD1.ORDERID
+                                                        WHERE O1.REQUIREDDATE BETWEEN '01/01/1996' AND '31/12/1996'
+                                                        GROUP BY O1.ORDERID
+)
+GROUP BY O.ORDERID, O.ORDERDATE
+ORDER BY SUMA_ORDEN DESC
+
+--para sacar el promedio de esas ordenes
+SELECT AVG(SUM((OD1.UNITPRICE * OD1.QUANTITY)*(1-OD1.DISCOUNT)))
+FROM ORDERS O1
+INNER JOIN ORDERDETAILS OD1 ON O1.ORDERID = OD1.ORDERID
+WHERE O1.REQUIREDDATE BETWEEN '01/01/1996' AND '31/12/1996'
+GROUP BY O1.ORDERID
+
+
+/* 32
+Realizar una consulta unificada dando el nombre de los 5 clientes quienes 
+más dinero gastaron junto con los 5 clientes que 
+menos gastaron, pero que esos clientes 
+hayan comprado todas sus órdenes entre las fechas 01/01/96 al 31/10/97
+Utilice una union entre los 5 que más gastaron y que menos gastaron
+*/
+SELECT *
+FROM(    
+    SELECT C.CONTACTNAME, SUM((OD.Quantity*OD.UnitPrice)*(1-OD.Discount)) AS TOTAL, 'Mas Gastaron' AS GASTO
+    FROM customers C 
+    INNER JOIN orders O ON O.CUSTOMERID = C.CUSTOMERID 
+    INNER JOIN orderdetails OD ON OD.OrderID = O.OrderID 
+    WHERE O.ORDERDATE BETWEEN '01/01/1996' AND '31/10/1997'
+    GROUP BY C.CONTACTNAME
+    ORDER BY SUM(OD.Quantity*OD.UnitPrice)  DESC
+    FETCH FIRST 5 ROW ONLY) mas_gastaron
+UNION ALL 
+SELECT *
+FROM (
+    SELECT C1.CONTACTNAME, SUM((OD1.Quantity*OD1.UnitPrice)*(1-OD1.Discount)) AS TOTAL, 'Menos Gastaron' AS GASTO
+    FROM customers C1 
+    INNER JOIN orders O1 ON O1.CUSTOMERID = C1.CUSTOMERID 
+    INNER JOIN orderdetails OD1 ON OD1.OrderID = O1.OrderID 
+    WHERE O1.ORDERDATE BETWEEN '01/01/1996' AND '31/10/1997'
+    GROUP BY CONTACTNAME  
+    ORDER BY SUM((OD1.Quantity*OD1.UnitPrice)-OD1.Discount) ASC
+    FETCH FIRST 5 ROW ONLY) menos_gastaron;
+
+
+   
+
+/* 33
+Dar el nombre y apellido del empleado y el conteo de órdenes que tienen hechas; 
+en donde tenga uno o más productos de todas las categorías que existe, y el promedio de 
+la suma del precio de todas sus órdenes supere los Q150.00
+*/
+SELECT E.FIRSTNAME ||' '|| E.LASTNAME NOMBRE_EMPLEADO, COUNT(O.ORDERID) TOTAL_ORDENES
+FROM EMPLOYEES E
+INNER JOIN ORDERS O ON E.EMPLOYEEID = O.EMPLOYEEID
+INNER JOIN ORDERDETAILS OD ON O.ORDERID = OD.ORDERID
+INNER JOIN PRODUCTS P ON OD.PRODUCTID  = P.PRODUCTID
+INNER JOIN CATEGORIES C ON P.CATEGORYID = C.CATEGORYID
+WHERE OD.ORDERID IN (
+            SELECT OD1.ORDERID
+            FROM ORDERDETAILS OD1 
+            HAVING AVG((OD1.UNITPRICE * OD1.QUANTITY)*(1-OD1.DISCOUNT)) > 150
+            GROUP BY OD1.ORDERID
+)
+GROUP BY E.FIRSTNAME ||' '|| E.LASTNAME
+HAVING COUNT(DISTINCT P.CATEGORYID) = ( SELECT COUNT(DISTINCT CATEGORYID) 
+                                        FROM CATEGORIES)
+ORDER BY COUNT(O.ORDERID) DESC
